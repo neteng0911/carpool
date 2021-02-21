2fdef28571c46a800f2c0e0070e84f1229045367
neteng0911 <e.matragos@gmail.com>
2021-02-21 18:49:26 +0200

    working new entry

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,8 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 #create_new_page
-def new(request,title,content):
-    return render(request,'encyclopedia/new.html', {"title":util.save_entry(title,content),"form":AddNewEntryForm()})
+def new(request):
+    # check if method is POST
+    if request.method=="POST":
+        form=AddNewEntryForm(request.POST)
+        if form.is_valid():
+            title=form.cleaned_data["title"]
+            content=form.cleaned_data["content"]
+            util.save_entry(title, content)
+        
+    return render(request,'encyclopedia/new.html',{"form":AddNewEntryForm()})
 #random_page
 def random(request):
     return render(request,'encyclopedia/random.html')
