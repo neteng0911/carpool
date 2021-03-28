7b95df1b19dce2be6ae27be3c92cb52adc11db5d
neteng0911 <e.matragos@gmail.com>
2021-03-28 11:59:20 +0300

    working random

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new",views.new, name="new"),
-    path("random",views.random, name="random"),
+    path("random_entry",views.random_entry, name="random_entry"),
     path("search_results",views.search_results, name="search_results"),
+    path("edit_entry/<str:title>",views.edit_entry, name="edit_entry"),
 
     path('<str:title>',views.entry, name='entry'),
 
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -3,6 +3,7 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from . import util
+import random
 import markdown2
 class AddNewEntryForm(forms.Form):
     title = forms.CharField(label="title")
@@ -37,6 +38,7 @@ def index(request):
 def new(request):
     # check if method is POST
     if request.method=="POST":
+
         form=AddNewEntryForm(request.POST)
         if form.is_valid():
             title=form.cleaned_data["title"]
@@ -51,13 +53,37 @@ def new(request):
         
     return render(request,'encyclopedia/new.html',{"form":AddNewEntryForm()})
 #random_page
-def random(request):
-    return render(request,'encyclopedia/random.html')
+def random_entry(request):
+    entries=util.list_entries()
+    num=len(entries)
+    print(num)
+    num1=random.randint(0,num-1)
+    print(num1)
+    title=entries[num1]
+    #shuffled=random.shuffle(entries)
+    #title=shuffled[0]
+
+    return entry(request,title)
 def entry(request,title):
     text=util.get_entry(title)
     html=markdown2.Markdown().convert(text)
 
     return render(request, 'encyclopedia/entry.html', {"entry":html})
+
+def edit_entry(request,title):
+    if request.method=="GET":
+        entry=util.get_entry(title)
+        return render(request, "encyclopedia/edit_entry.html", {"content": entry,"title":title})
+
+    if request.method=="POST":
+        content=request.POST["content"]
+
+        util.save_entry(content,title)
+
+        return entry(request,title)
+
+    return render(request, "encyclopedia/edit_entry.html")
+
 
 
 
