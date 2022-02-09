c82e8839c662be3dc033eea30cdd88f9893a1be0
neteng0911 <e.matragos@gmail.com>
2022-02-09 19:06:48 +0200

    index

--- project5/Capstone/models.py
+++ project5/Capstone/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from django.utils.timezone import now
+
+from django.utils import timezone
+
+class User(AbstractUser):
+    email=models.EmailField(max_length=254)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+
+
+    def __str__(self):
+        return f"{self.username}"
+        #return f"{self.username}, {self.email}"
--- project5/project5/settings.py
+++ project5/project5/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "Capstone.User"
 # Application definition
 
 INSTALLED_APPS = [
+    'Capstone',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
