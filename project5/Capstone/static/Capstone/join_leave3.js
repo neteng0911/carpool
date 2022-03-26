29a968983fc06ac3f6f4c78dab5d1cf341199c78
neteng0911 <e.matragos@gmail.com>
2022-03-26 23:31:38 +0200

    trying max passengers

--- project5/Capstone/static/Capstone/join_leave3.js
+++ project5/Capstone/static/Capstone/join_leave3.js
@@ -5,7 +5,27 @@
        var sortbtn = document.querySelector('#sortbtn');
         sortbtn.addEventListener('click', sort);
 
+      document.querySelectorAll('#route').forEach((element) => {
 
+
+
+       var route_id = element.dataset.id;
+       console.log(route_id)
+
+
+
+        var fin=route["fin"];
+        var fin_set = route['fin_set'];
+
+       console.log(fin)
+       if (fin == 1 || fin_set == 1) {document.querySelector('.likes').style.display='none';
+
+       }
+       else{document.querySelector('.likes').style.display='block';
+
+       }
+       }
+       )
 
 
 
@@ -18,24 +38,11 @@
 
             var route_id = element.dataset.id;
 
-            fetch(`/route/${route_id}`)
-                .then(response => response.json())
-                .then(route => {
-
-                if (route.error) {
-                    console.log(route.error);
-                    alert(route.error)
-
-        }
-
-            var no_pass= parseFloat(document.querySelector(`#thepassengercounter_${route_id}`).innerHTML);
-            var max_pass = route["no_pass"];
-            console.log(route_id);
-            console.log(max_pass);
 
 
+            console.log(route_id)
 
-       while (no_pass < max_pass){
+
 
             if(element.title == "Join")
             {
@@ -43,49 +50,28 @@
 
                 element.style.color = "#B22222";
                 element.style.border = "1px solid #B22222";
-                element.title="Leave";
-                element.innerHTML="Leave";
+                element.title="Leave"
+                element.innerHTML="Leave"
                 document.querySelector(`#thepassengercounter_${route_id}`).innerHTML++;
                 join_route(route_id);
                 est_cost(route_id);
 
             }
-            else if (element.title == "Leave"){
-               console.log(element);
+            else if (element.title == "Leave")
+            {   console.log(element)
 
-                element.title="Join";
-                element.innerHTML="Join";
+                element.title="Join"
+                element.innerHTML="Join"
                 element.style.color = "#228B22";
                 element.style.border = "1px solid  #228B22";
                 document.querySelector(`#thepassengercounter_${route_id}`).innerHTML--;
                 leave_route(route_id);
                 est_cost(route_id);
-
-                }
-
-
-
-
-
-       else{
-            console.log(element);
-
-                element.style.color = "#B22222";
-                element.style.border = "1px solid #B22222";
-                element.title="Leave";
-                element.innerHTML="Leave";
-                document.querySelector(`#thepassengercounter_${route_id}`).innerHTML--;
-                leave_route(route_id);
-                est_cost(route_id);
-               }
-       }
-
-
-        })
-    }});
+            }
+        }
+    });
 
     function sort(){
-    alert('sorting')
     document.querySelectorAll('#route').forEach((element) => {
 
 
@@ -146,15 +132,15 @@ fetch(`/route/${route_id}`)
        var destination=route["destination"];
        var init_cost=route["cost"];
        console.log(init_cost)
-       var no_passengers= parseFloat(document.querySelector(`#thepassengercounter_${route_id}`).innerHTML);
+       var no_pass= parseFloat(document.querySelector(`#thepassengercounter_${route_id}`).innerHTML);
 
-       console.log(no_passengers);
-       if (no_passengers ===0){
+       console.log(no_pass);
+       if (no_pass ===0){
        var cost=init_cost;
        document.querySelector(`#costpp_${route_id}`).innerHTML = cost+'$';
 }
        else{
-       var cost =init_cost/no_passengers;
+       var cost =init_cost/no_pass;
        document.querySelector(`#costpp_${route_id}`).innerHTML = cost+'$';
 
 }
