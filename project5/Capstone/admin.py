483964b58501afde20972a24680bf73824d6c435
neteng0911 <e.matragos@gmail.com>
2022-04-19 18:54:50 +0300

    ver0

--- project5/Capstone/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Route, Comment, User, Message
-
-# Register your models here.
-
-admin.site.register(Route)
-admin.site.register(Comment)
-admin.site.register(User)
-admin.site.register(Message)
--- project5/Capstone/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CapstoneConfig(AppConfig):
-    name = 'Capstone'
