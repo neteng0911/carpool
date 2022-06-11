0ad8b43b5a7dfbb8f68ee24c9b5e4b47ffea5b84
neteng0911 <e.matragos@gmail.com>
2022-06-11 17:57:54 +0300

    fixing issues heroku3

--- project5/settings.py
+++ project5/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
