86cf5df93368159c8d4a8e7f92cc0fc16e04e0f2
neteng0911 <e.matragos@gmail.com>
2022-06-11 17:55:48 +0300

    fixing issues heroku

--- project5/settings.py
+++ project5/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-form decouple import config
+from decouple import config
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
